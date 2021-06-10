2d8d48eafbe95c8e8f29cb74f9890edcfb4bb8fc
Amanuel Ephem <amanuelephrem776@gmail.com>
2021-06-09 21:50:25 -0400

    Added an iterator of the min and max views to the priority queue

--- Tests/PriorityQueueTests/PriorityQueueTests.swift
+++ Tests/PriorityQueueTests/PriorityQueueTests.swift
@@ -291,4 +291,24 @@ final class PriorityQueueTests: XCTestCase {
         XCTAssertEqual(queue.removeMin(), 10)
         XCTAssertEqual(queue.removeMax(), 11)
     }
+    
+    func test_sequenceConformance() {
+        var queue = PriorityQueue<Int>()
+        for val in (0...50).shuffled() {
+            queue.insert(val)
+        }
+
+        var increment = 0
+        for val in queue.ascending {
+            XCTAssertEqual(increment, val)
+            increment += 1
+        }
+
+        increment = 50
+        for val in queue.descending {
+            XCTAssertEqual(increment, val)
+            increment -= 1
+        }
+        
+    }
 }
