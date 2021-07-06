5ffd7336df4e0480020d52546e4c517c979d93dd
Tyler Stromberg <stromberg@squareup.com>
2021-07-05 22:25:31 -0700

    Reintroduce PriorityQueue type as a wrapper around MinMaxHeap
    It's generic around a Value and a Priority, so contents no longer have
    to conform to Comparable directly. It also keeps track of insertion
    order, so elements of the same priority will be dequeued in FIFO order.

--- Benchmarks/swift-collections-benchmark/main.swift
+++ Benchmarks/swift-collections-benchmark/main.swift
@@ -20,6 +20,7 @@ benchmark.addDequeBenchmarks()
 benchmark.addOrderedSetBenchmarks()
 benchmark.addOrderedDictionaryBenchmarks()
 benchmark.addHeapBenchmarks()
+benchmark.addPriorityQueueBenchmarks()
 benchmark.addCFBinaryHeapBenchmarks()
 benchmark.addCppBenchmarks()
 
